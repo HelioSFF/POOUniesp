b9986b33abb51a11bd49bfe28b54be63e640a11e
Hélio de Siqueira <heliosiqueiraff@gmail.com>
2025-05-15 22:26:16 -0300

    Delete ProjetoJava/src directory

--- ProjetoJava/src/repository/IPacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Paciente;
-import java.util.List;
-
-public interface IPacienteRepository {
-    void salvar(Paciente paciente);
-    List<Paciente> listarTodos();
-}
